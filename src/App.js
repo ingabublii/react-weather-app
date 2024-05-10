c52219586f2b35822a85b012316056dcc36a1afe
ingabublii <ingaasmiles96@gmail.com>
2024-05-10 17:18:44 +0300

    corrected some errors

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="Container">
-        <Weather />
+        <Weather defaultCity="Kyiv"/>
         <footer>
           This project was coded by{" "} 
           <a href="https://github.com/ingabublii">Inga Bublii</a>{" "} and is {" "} <a href="https://github.com/ingabublii/react-weather-app">open sourced on Github</a>
--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
             humidity: response.data.main.humidity,
             city: response.data.name,
             date: new Date(response.data.dt * 1000),
-            icon: response.data.condition.icon,
+            icon: response.data.weather[0].icon,
             description: response.data.weather[0].description
 
         });
