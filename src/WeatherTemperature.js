b8b6f7190e93f07495171f3ed20e71d6f93c97f3
ingabublii <ingaasmiles96@gmail.com>
2024-05-11 16:32:49 +0300

    added unit info

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 export default function WeatherTemperature(props) {
-  return (
-    <div className="WeatherTemperature">
-      <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">°C</span>
-    </div>
-  );
+const [unit, setUnit] = useState("celsius");
+function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahreheit");
+}
+
+function showCelsius(event){
+    event.preventDefault();
+    setUnit("celsius");
+}
+
+function fahreheit (){
+    return(props.celsius * 9) /5 + 32;
+}
+
+    if (unit === "celsius") {
+    return (
+        <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(fahreheit())}</span>
+        <span className="unit">
+                °F | {""} 
+            <a href="/" onClick={showFahrenheit}> 
+                °C
+             </a>
+        </span>
+      </div>
+    );
+} else {
+    return (
+        <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+                °C | {""} 
+                <a href="/" onClick={showCelsius}> 
+                °F
+                </a> 
+                
+        </span>
+      </div>
+    );
+}
+  
+
 }
