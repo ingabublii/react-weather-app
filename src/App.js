2a1a913c1e98ced200df3e6b5f5af69dd34b0fcb
ingabublii <ingaasmiles96@gmail.com>
2024-05-11 16:35:58 +0300

    added some changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         <Weather defaultCity="Kyiv"/>
         <footer>
           This project was coded by{" "} 
-          <a href="https://github.com/ingabublii">Inga Bublii</a>{" "} and is {" "} <a href="https://github.com/ingabublii/react-weather-app">open sourced on Github</a>
+          <a href="https://github.com/ingabublii">Inga Bublii</a>{" "} and is {" "} <a href="https://github.com/ingabublii/react-weather-app">open sourced on Github</a> and hosted on <a href="https://comfy-gaufre-d3f4bc.netlify.app/">Netlify</a>
         </footer>
       </div>
     </div>
