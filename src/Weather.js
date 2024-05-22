0df7234aaebabb23bd2235253a1f49cd9af026ff
ingabublii <ingaasmiles96@gmail.com>
2024-05-22 11:24:54 +0300

    added WeatherForecast and WeatherForecast styles

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import axios from "axios";
 import "./Weather.css";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 
 
 export default function Weather(props) {
@@ -47,6 +48,7 @@ export default function Weather(props) {
                 
                 </form>
                 <WeatherInfo data={weatherData} />
+                <WeatherForecast />
             </div>
         );
    
